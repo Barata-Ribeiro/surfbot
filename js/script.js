cf577de9a9d908ae2322ee8639702f0f98ca4a89
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-02-25 12:57:52 -0300

    Adicionado o Media Query mobile

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ if (horaAtual > horaAbertura && horaAtual < horaFechamento) {
     // A loja está fechada
     document.querySelector('.horario').classList.add('fechado');
 }
+
+const menuLinks = document.querySelectorAll('nav a[href*="#"]');
+menuLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetID = link.getAttribute('href');
+        const targetElement = document.querySelector(targetID);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
