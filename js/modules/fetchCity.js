0757ab46188bfafd92780e77ef579c01d34fe705
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-05 11:19:50 -0300

    fetchCity refatorado

--- js/modules/fetchCity.js
+++ js/modules/fetchCity.js
@@ -1,24 +1,37 @@
-export default function initFetchCity() {
-    const estadoSelect = document.getElementById('estado');
-    const cidadeSelect = document.getElementById('cidade');
+export default class FetchCity {
+    constructor(select1, select2) {
+        this.estadoSelect = document.getElementById(select1);
+        this.cidadeSelect = document.getElementById(select2);
 
-    estadoSelect.addEventListener('change', (event) => {
-        const state = event.target.value;
-        const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios?orderBy=nome`;
+        if (!this.estadoSelect || !this.cidadeSelect) {
+            throw new Error('Elementos estado/cidade não encontrados!');
+        }
+    }
 
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                cidadeSelect.innerHTML = '<option value="">Selecione a Cidade</option>';
-                data.forEach((cidade) => {
-                    const option = document.createElement('option');
-                    option.value = cidade.nome;
-                    option.text = cidade.nome;
-                    cidadeSelect.add(option);
+    FetchCityEventListener() {
+        this.estadoSelect.addEventListener('change', (event) => {
+            const state = event.target.value;
+            const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios?orderBy=nome`;
+
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.cidadeSelect.innerHTML = '<option value="">Selecione a Cidade</option>';
+                    data.forEach((cidade) => {
+                        const option = document.createElement('option');
+                        option.value = cidade.nome;
+                        option.text = cidade.nome;
+                        this.cidadeSelect.add(option);
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
                 });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    });
+        });
+    }
+
+    init() {
+        this.FetchCityEventListener();
+        return this;
+    }
 }
